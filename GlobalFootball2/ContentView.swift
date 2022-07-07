6be7ba0d338eb9904484d4d709a32dac6f2b769f
Matthew Lewis <lewimb17@wfu.edu>
2022-07-07 15:25:12 -0500

    Initial Commit

---
+++ GlobalFootball2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlobalFootball2
+//
+//  Created by Matthew Lewis on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlobalFootball2/GlobalFootball2App.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalFootball2App.swift
+//  GlobalFootball2
+//
+//  Created by Matthew Lewis on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GlobalFootball2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
